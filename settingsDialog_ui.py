05158e99ecb38a72c880c630dbb3089d37ee7324
HennaSalla <51696@edu.raseko.fi>
2024-12-10 09:16:49 +0200

    .

--- settingsDialog_ui.py
+++ settingsDialog_ui.py
@@ -16,7 +16,7 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QApplication, QDialog, QLabel, QLineEdit,
-    QPushButton, QSizePolicy, QVBoxLayout, QWidget)
+    QPushButton, QSizePolicy, QVBoxLayout, QWidget).
 
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
