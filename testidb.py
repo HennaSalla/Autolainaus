e58f87acaaa89ba8ba6b13d8b6bd38dbc041d859
HennaSalla <51696@edu.raseko.fi>
2024-12-11 15:06:01 +0200

    11.12

--- testidb.py
+++ testidb.py
@@ -11,7 +11,7 @@ except Exception as e:
 finally:
     if connection:
         cursor.close()
-        connection.close()
+        connection.close() 
 
 try:
     connectionString = "dbname=testi user=user password=salasana host=localhost port=5432"
