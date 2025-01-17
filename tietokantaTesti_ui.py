272bd0893d7ca1453305ad34c7e9d30282379232
HennaSalla <51696@edu.raseko.fi>
2025-01-17 13:33:27 +0200

    17.1.2025 muokattu testi tietokantaa

--- tietokantaTesti_ui.py
+++ tietokantaTesti_ui.py
@@ -11,46 +11,99 @@
 from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
     QMetaObject, QObject, QPoint, QRect,
     QSize, QTime, QUrl, Qt)
-from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
-    QFont, QFontDatabase, QGradient, QIcon,
-    QImage, QKeySequence, QLinearGradient, QPainter,
-    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtGui import (QAction, QBrush, QColor, QConicalGradient,
+    QCursor, QFont, QFontDatabase, QGradient,
+    QIcon, QImage, QKeySequence, QLinearGradient,
+    QPainter, QPalette, QPixmap, QRadialGradient,
+    QTransform)
 from PySide6.QtWidgets import (QApplication, QLabel, QLineEdit, QMainWindow,
-    QMenuBar, QPushButton, QSizePolicy, QStatusBar,
-    QWidget)
+    QMenu, QMenuBar, QPushButton, QSizePolicy,
+    QStatusBar, QWidget)
+import testPictures_rc
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
         MainWindow.resize(800, 600)
+        icon = QIcon(QIcon.fromTheme(u"preferences-desktop-accessibility"))
+        MainWindow.setWindowIcon(icon)
+        MainWindow.setStyleSheet(u"background-color: rgb(223, 32, 112);")
+        self.actionLopeta = QAction(MainWindow)
+        self.actionLopeta.setObjectName(u"actionLopeta")
+        icon1 = QIcon(QIcon.fromTheme(u"application-exit"))
+        self.actionLopeta.setIcon(icon1)
+        self.actionKalle = QAction(MainWindow)
+        self.actionKalle.setObjectName(u"actionKalle")
+        self.actionVille = QAction(MainWindow)
+        self.actionVille.setObjectName(u"actionVille")
+        self.actionJaana = QAction(MainWindow)
+        self.actionJaana.setObjectName(u"actionJaana")
+        self.actionMikko = QAction(MainWindow)
+        self.actionMikko.setObjectName(u"actionMikko")
+        self.actionTallenna_nimell = QAction(MainWindow)
+        self.actionTallenna_nimell.setObjectName(u"actionTallenna_nimell")
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
         self.firstNameLineEdit = QLineEdit(self.centralwidget)
         self.firstNameLineEdit.setObjectName(u"firstNameLineEdit")
         self.firstNameLineEdit.setGeometry(QRect(10, 40, 113, 22))
+        self.firstNameLineEdit.setStyleSheet(u"color: rgb(255, 255, 255);")
         self.firstNameLabel = QLabel(self.centralwidget)
         self.firstNameLabel.setObjectName(u"firstNameLabel")
         self.firstNameLabel.setGeometry(QRect(10, 20, 81, 16))
+        self.firstNameLabel.setStyleSheet(u"color: rgb(255, 255, 255);")
         self.lastNameLineEdit = QLineEdit(self.centralwidget)
         self.lastNameLineEdit.setObjectName(u"lastNameLineEdit")
         self.lastNameLineEdit.setGeometry(QRect(150, 40, 113, 22))
         self.lastNameLabel = QLabel(self.centralwidget)
         self.lastNameLabel.setObjectName(u"lastNameLabel")
         self.lastNameLabel.setGeometry(QRect(150, 20, 61, 16))
+        self.lastNameLabel.setStyleSheet(u"color: rgb(255, 255, 255);")
         self.savePushButton = QPushButton(self.centralwidget)
         self.savePushButton.setObjectName(u"savePushButton")
         self.savePushButton.setGeometry(QRect(190, 80, 75, 24))
         self.savePushButton.setCursor(QCursor(Qt.CursorShape.PointingHandCursor))
         self.savePushButton.setToolTipDuration(3000)
+        self.savePushButton.setStyleSheet(u"color: rgb(255, 255, 255);\n"
+"background-color: rgb(75, 188, 197);")
+        self.studentPictureLabel = QLabel(self.centralwidget)
+        self.studentPictureLabel.setObjectName(u"studentPictureLabel")
+        self.studentPictureLabel.setGeometry(QRect(10, 110, 231, 221))
+        self.studentPictureLabel.setPixmap(QPixmap(u":/png/student.png"))
+        self.studentPictureLabel.setScaledContents(False)
+        self.keysPictureLabel = QLabel(self.centralwidget)
+        self.keysPictureLabel.setObjectName(u"keysPictureLabel")
+        self.keysPictureLabel.setGeometry(QRect(230, 150, 231, 161))
+        self.keysPictureLabel.setPixmap(QPixmap(u":/png/keys.png"))
+        self.teacherPictureLabel = QLabel(self.centralwidget)
+        self.teacherPictureLabel.setObjectName(u"teacherPictureLabel")
+        self.teacherPictureLabel.setGeometry(QRect(440, 130, 191, 211))
+        self.teacherPictureLabel.setPixmap(QPixmap(u"teacher.png"))
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
         self.menubar.setGeometry(QRect(0, 0, 800, 33))
+        self.menubar.setStyleSheet(u"background-color: rgb(0, 33, 72);\n"
+"color: rgb(255, 255, 255);")
+        self.menuTiedosto = QMenu(self.menubar)
+        self.menuTiedosto.setObjectName(u"menuTiedosto")
+        self.menuEdelliset = QMenu(self.menuTiedosto)
+        self.menuEdelliset.setObjectName(u"menuEdelliset")
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
+        self.statusbar.setStyleSheet(u"background-color: rgb(243, 84, 148);")
         MainWindow.setStatusBar(self.statusbar)
+
+        self.menubar.addAction(self.menuTiedosto.menuAction())
+        self.menuTiedosto.addAction(self.actionLopeta)
+        self.menuTiedosto.addAction(self.menuEdelliset.menuAction())
+        self.menuTiedosto.addAction(self.actionTallenna_nimell)
+        self.menuEdelliset.addAction(self.actionKalle)
+        self.menuEdelliset.addAction(self.actionVille)
+        self.menuEdelliset.addAction(self.actionJaana)
+        self.menuEdelliset.addAction(self.actionMikko)
 
         self.retranslateUi(MainWindow)
 
@@ -59,11 +112,25 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.actionLopeta.setText(QCoreApplication.translate("MainWindow", u"Lopeta", None))
+#if QT_CONFIG(shortcut)
+        self.actionLopeta.setShortcut(QCoreApplication.translate("MainWindow", u"Ctrl+Q", None))
+#endif // QT_CONFIG(shortcut)
+        self.actionKalle.setText(QCoreApplication.translate("MainWindow", u"Kalle", None))
+        self.actionVille.setText(QCoreApplication.translate("MainWindow", u"Ville", None))
+        self.actionJaana.setText(QCoreApplication.translate("MainWindow", u"Jaana", None))
+        self.actionMikko.setText(QCoreApplication.translate("MainWindow", u"Mikko", None))
+        self.actionTallenna_nimell.setText(QCoreApplication.translate("MainWindow", u"Tallenna nimell\u00e4...", None))
         self.firstNameLabel.setText(QCoreApplication.translate("MainWindow", u"Puhuttelunimi", None))
         self.lastNameLabel.setText(QCoreApplication.translate("MainWindow", u"Sukunimi", None))
 #if QT_CONFIG(tooltip)
         self.savePushButton.setToolTip(QCoreApplication.translate("MainWindow", u"<html><head/><body><p><span style=\" font-size:11pt;\">Tallenetaan henkil\u00f6n tiedot tietokantaan</span></p></body></html>", None))
 #endif // QT_CONFIG(tooltip)
         self.savePushButton.setText(QCoreApplication.translate("MainWindow", u"Tallenna", None))
+        self.studentPictureLabel.setText("")
+        self.keysPictureLabel.setText("")
+        self.teacherPictureLabel.setText("")
+        self.menuTiedosto.setTitle(QCoreApplication.translate("MainWindow", u"Tiedosto", None))
+        self.menuEdelliset.setTitle(QCoreApplication.translate("MainWindow", u"Edelliset", None))
     # retranslateUi
 
