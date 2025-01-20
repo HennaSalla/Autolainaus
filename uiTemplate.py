ddb7ea1831a8663939c7c1ef4f67b35f081222cb
HennaSalla <51696@edu.raseko.fi>
2025-01-20 10:26:34 +0200

    20.1.2025 lisätty komentti

--- uiTemplate.py
+++ uiTemplate.py
@@ -8,6 +8,8 @@ import os # Polkumääritykset
 import sys # Käynnistysargumentit
 
 from PySide6 import QtWidgets # Qt-vimpaimet
+
+# Tuodaan käyttöliittymän Pythoniksi käänetty tiedosto, korvaa mainwindow_ui todellisella tiedoston nimellä
 from mainWindow_ui import Ui_MainWindow # Käännetyn käyttöliittymän luokka
 
 # Määritellään luokka, joka perii QMainWindow- ja Ui_MainWindow-luokan
