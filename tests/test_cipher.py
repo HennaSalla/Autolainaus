df1cbc355ad9e35560cd173a32d0ec60f74ce8b6
HennaSalla <51696@edu.raseko.fi>
2025-01-29 12:27:04 +0200

    29.1.2025 lisätty komentteja

--- tests/test_cipher.py
+++ tests/test_cipher.py
@@ -4,11 +4,13 @@
 import pytest # Järjestelmätason virheiden testaus
 from lendingModules import cipher # Testattavan moduulin lataus
 
+# Tehdään testaamista varten salakirjoitettu teksti
 plainText = b'Selkokieliteksti'
 key = b'oqjptt7iNxAnpF4DnKyVmcSv9mu3feeVChxBHZijMsI='
 chipherEngine = cipher.createChipher(key)
 cryptoText = cipher.encrypt(chipherEngine, plainText)
 
+# Testataan, että salakirjoitettu teksti muuttuu takaisin selkokieliseksi
 def test_decrypt():
     assert cipher.decrypt(chipherEngine, cryptoText, True) == plainText 
 
