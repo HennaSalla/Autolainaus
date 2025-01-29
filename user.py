c5d5937889929664c01d1232a8fb118bd9d9f05b
HennaSalla <51696@edu.raseko.fi>
2025-01-29 15:01:13 +0200

    29.1.2025 muokattuu käyttöliittymää

--- user.py
+++ user.py
@@ -26,7 +26,9 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         # Kutsutaan käyttöliittymän muodostusmetodia setupUi
         self.ui.setupUi(self)
 
-        # Piilotetaan objecteja
+        # Ohjelmaa käynnistäessä piilotetaan tarpeettomat elementit
+        self.ui.returnCarPushButton.show()
+        self.ui.takeCarPushButton.show()
         self.ui.soundOnPushButton.hide()
         self.ui.borrowerLabel.hide()
         self.ui.humanLabel.hide()
@@ -41,15 +43,15 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.ui.clockPictureLabel.hide()
         self.ui.hourLabel.hide()
         self.ui.goBackPushButton.hide()
+        self.ui.okPushButton.hide()
+
+
 
         # OHJELMOIDUT SIGNAALIT
         # ---------------------
 
         # Kuin Lainaa auto painiketta on painettu kutsutaan metodia takeCar
         self.ui.takeCarPushButton.clicked.connect(self.takeCar)
-
-        # Kuin Palauta auto painiketta on painettu kutsutaan metodia returnCar
-        self.ui.returnCarPushButton.clicked.connect(self.returnCar)
 
         # Kuina ajokortti on luettu kutsutaan showKeys metodia
         self.ui.licenseLineEdit.returnPressed.connect(self.showKeys)
@@ -60,19 +62,26 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         # Komennot kutsumaan metodeja jotka halitsee ääni nappulaa
         self.ui.soundOffPushButton.clicked.connect(self.muteSound)
         self.ui.soundOnPushButton.clicked.connect(self.takeSound)
+
+        # Kuin Palauta auto painiketta on painettu kutsutaan metodia returnCar
+        self.ui.returnCarPushButton.clicked.connect(self.returnCar)
         
     # OHJELMOIDUT SLOTIT
     # ------------------
+
+    # Kuin Aloita lainaus nappia on painettu nämä componentit tulee esiin tai piiloutuu
     def takeCar(self):
         self.ui.borrowerLabel.show()
         self.ui.humanLabel.show()
         self.ui.goBackPushButton.show()
+        self.ui.okPushButton.show()
         self.ui.licenseLineEdit.show()
         self.ui.licenseLineEdit.setFocus()
         self.ui.returnCarPushButton.hide()
         self.ui.takeCarPushButton.hide()
         self.ui.statusbar.showMessage('Lue ajokortin viivakoodi', 6000)
 
+    # Ajokortin lukemisen jälkeen nämä komponentint tulevat essin
     def showKeys(self):
         self.ui.nameLabel.show()
         self.ui.carTakeLabel.show()
@@ -81,6 +90,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.ui.keysLineEdit.setFocus()
         self.ui.statusbar.showMessage('Lue avaimen viivakoodi', 6000)
 
+    # Kuin avaimen viivakoodi on luettu nämä komponentit tulevat essin
     def showTime(self):
         self.ui.carInfoLabel.show()
         self.ui.calenderLabel.show()
@@ -88,18 +98,28 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.ui.clockPictureLabel.show()
         self.ui.hourLabel.show()
 
+
+    # mykistäessä nämä komponentit tulevat esiin tai piilotetaan
     def muteSound(self):
         self.ui.soundOnPushButton.show()
         self.ui.soundOffPushButton.hide()
 
+    # Kuin ääni palautetaan nämä komponentitn tulevat esiin tai piilotetaan
     def takeSound(self):
         self.ui.soundOffPushButton.show()
-        self.ui.soundOnPushButton.show
+        self.ui.soundOnPushButton.hide()
 
+    # Kuin aloita palautus nappia on painettu nämä komponentit tulevat näkyviin tai piiloutuu
     def returnCar(self):
-        pass
-
-    # Muutetaan tulostettuLabel:n sisältö: teksti ja väri
+        self.ui.takeCarPushButton.hide()
+        self.ui.returnCarPushButton.hide()
+        self.ui.carTakeLabel.show()
+        self.ui.carKeysLabel.show()
+        self.ui.keysLineEdit.show()
+        self.ui.keysLineEdit.setFocus()
+        self.ui.goBackPushButton.show()
+        self.ui.okPushButton.show()
+        self.ui.statusbar.showMessage('Lue avaimen viivakoodi', 6000)
     
 
     # Avataan MessageBox
